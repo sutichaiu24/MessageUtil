8db1530af412b9a673703d921b510d5ab9328693
sutichaiu24 <oh@isecthailand.com>
2019-04-29 14:11:26 +0700

    Update TestMessageUtil.java

--- src/test/java/TestMessageUtil.java
+++ src/test/java/TestMessageUtil.java
@@ -16,7 +16,7 @@ public class TestMessageUtil {
     @Test
     public void testSalutationMessage() {
         System.out.println("Inside testSalutationMessage()");
-        message = "Hi!" + "Robert";
+        message = "Robert";
         assertEquals(message,messageUtil.salutationMessage());
     }
 }
